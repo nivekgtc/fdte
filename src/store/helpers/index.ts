503fa2caec16125762376f2385cf08d5db3ae0c3
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2023-04-25 16:32:10 -0300

    feat(store-redux): create initial configurations of redux container
    add initial actions and mappers for pokemon ability and create his reducer

---
+++ src/store/helpers/index.ts
@@ -0,0 +1,2 @@
+export * from './create-hydrated-slice'
+export * from './reset-state'
---
+++ src/store/store.ts
@@ -0,0 +1,20 @@
+import { createWrapper } from 'next-redux-wrapper'
+import { configureStore } from '@reduxjs/toolkit'
+import { pokemonSlice } from './features/pokemon/slices/pokemon-slice'
+import { pokemonSliceName, PokemonSliceState } from './features/pokemon/types'
+
+export type AppDispatch = ReturnType<typeof createStore>['dispatch']
+export type AppState = ReturnType<ReturnType<typeof createStore>['getState']>
+
+export type StoreProps = {
+  pokemonSlice: PokemonSliceState
+}
+
+const createStore = () =>
+  configureStore<StoreProps>({
+    reducer: {
+      [pokemonSliceName]: pokemonSlice.reducer
+    }
+  })
+
+export const ReduxWrapper = createWrapper(createStore)
