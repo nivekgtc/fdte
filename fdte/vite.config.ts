413f804096fb5d0c51fd622a99d2fc924a29ea88
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2023-04-27 10:01:36 -0300

    build: fix build imports, and configure vite to use @decorators

--- fdte/vite.config.ts
+++ fdte/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import typescript from '@rollup/plugin-typescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    typescript()
+  ],
+  build: {
+    rollupOptions: {
+      input: ['./src/main.ts', './src/types/index.d.ts'],
+    },
+  },
 })
