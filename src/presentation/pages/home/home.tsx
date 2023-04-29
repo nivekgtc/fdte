6e07ca788d2ca4c5f15659bb95baf4d4e01ce469
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2023-04-28 22:01:05 -0300

    feat: finish homepage

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,11 +1,16 @@
-import { Button } from "~/presentation/components";
-import * as S from "./styled";
+import { useNavigate } from 'react-router-dom';
+import { Button } from '~/presentation/components';
+import * as S from './styled';
 
-const HomePage = () => (
-  <S.HomeWrapper>
-    <S.PokemonImage />
-    <Button text="Start" />
-  </S.HomeWrapper>
-);
+const HomePage = () => {
+	const navigate = useNavigate();
+
+	return (
+		<S.HomeWrapper>
+			<S.PokemonImage />
+			<Button text="Start" onClick={() => navigate('/map')} />
+		</S.HomeWrapper>
+	);
+};
 
 export default HomePage;
