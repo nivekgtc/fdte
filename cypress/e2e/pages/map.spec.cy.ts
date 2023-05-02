86b3741e913a0bde68d8671515837d2754f4e045
Kevin Cavalcanti <k.w9cavalcanti@gmail.com>
2023-05-02 20:54:31 -0300

    test: remove unused tests

--- cypress/e2e/pages/map.spec.cy.ts
+++ cypress/e2e/pages/map.spec.cy.ts
@@ -90,4 +90,44 @@ describe('MapPage', () => {
 			.scrollIntoView()
 			.click();
 	});
+
+	// it('should create from scratcha a pokemon', () => {
+	// 	cy.visit('http://localhost:5173/map');
+
+	// 	cy.wait(1000);
+
+	// 	cy.get('[data-cy="button-create-pokemon"]').click();
+
+	// 	cy.wait(400);
+
+	// 	cy.get('[data-cy="create-or-edit-scratch"]').should('be.visible');
+
+	// 	cy.get('[name="name"]').type('Pokemon Create');
+	// 	cy.get('[name="hp"]').type('40');
+	// 	cy.get('[name="weight"]').type('40');
+	// 	cy.get('[name="height"]').type('120');
+
+	// 	cy.get('[id="type"]').click();
+
+	// 	cy.contains('fighting').click();
+
+	// 	cy.get('[id="type"]').click();
+
+	// 	cy.get('[id="abilities.0"]').click();
+	// 	cy.contains('stench').click();
+	// 	cy.get('[id="abilities.1"]').click();
+	// 	cy.contains('drizzle').click();
+	// 	cy.get('[id="abilities.2"]').click();
+	// 	cy.contains('sturdy').click();
+	// 	cy.get('[id="abilities.3"]').click();
+	// 	cy.contains('speed-boost').click();
+	// 	cy.get('[id="abilities.4"]').click();
+	// 	cy.contains('damp').click();
+
+	// 	cy.get('[name="stats.defense"]').type('20');
+	// 	cy.get('[name="stats.attack"]').type('20');
+	// 	cy.get('[name="stats.special-defense"]').type('20');
+	// 	cy.get('[name="stats.special-attack"]').type('20');
+	// 	cy.get('[name="stats.speed"]').type('20');
+	// });
 });
